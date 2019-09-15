73f02f53e5777b13edf80f8c3ec5d8704f3a1091
Rogier <tujhybr728@mail.ru>
2019-09-15 22:38:02 +0300

    Проверил 2 версии ссылки, но вроде всё выполнил

---
+++ WebStore/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseMvcWithDefaultRoute();
+            // Производим конфигурацию инфраструктуры MVC
+            app.UseMvc(routes =>
+            {
+                // Добавляем обработчик маршрута по умолчанию
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+                // Маршрут по умолчанию состоит из трёх частей разделённых “/”
+                // Первой частью указывается имя контроллера,
+                // второй - имя действия (метода) в контроллере,
+                // третей - опциональный параметр с именем “id”
+                // Если чать не указана - используются значения по умолчанию:
+                // для контроллера имя “Home”,
+                // для действия - “Index”
+            });
+
+            var helloMessage = Configuration["CustomHelloWorld"];
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(helloMessage);
+            });
+        }
+    }
+}
